410aab653f7d20a0d1f0fb88a3969d6b4dd11771
YanX <typhoon322@qq.com>
2018-04-12 18:08:31 +0800

    实现上传demo

--- src/components/UploadPage.vue
+++ src/components/UploadPage.vue
@@ -9,60 +9,112 @@
 <script>
 import ImageCompressor from 'image-compressor.js';
 
-var qiniu = require('qiniu-js');
+const qiniu = require('qiniu-js');
+const requestHeaders = {
+	'Origin': 'http://localhost',
+  'Accept': 'application/json',
+  'Content-Type': 'application/x-www-form-urlencoded',
+  'Accept-Encoding': 'gzip',
+  'Host': '192.168.1.107',
+};
+const timeoutErr = {
+  code: 1,
+  msg: '请求超时',
+};
+const catchErr = {
+  code: 1,
+  msg: '网络异常',
+};
+
 export default {
 	methods: {
 		getFile(e) {
-    const file = e.target.files[0];
-    let errorMsg = null;
-    if (this.fileType === 'image' && !/image\/\w+/.test(file.type)) {
-      errorMsg = '请上传图片类型的文件';
-    }
+	    const file = e.target.files[0];
+	    let errorMsg = null;
+	    if (this.fileType === 'image' && !/image\/\w+/.test(file.type)) {
+	      errorMsg = '请上传图片类型的文件';
+	    }
 
-    if (errorMsg) {
-      this.$bus.$emit('showPopup', {
-        name: 'tip',
-        data: errorMsg,
-      });
-      e.target.value = null;
-      return;
-    }
-    if (this.fileType === 'image') {
-      const imageCompressor = new ImageCompressor();
-      imageCompressor.compress(file, { quality: 1 })
-        .then((result) => {
-          if (result.size > 8 * 1024 * 1024) {
-            this.$bus.$emit('showPopup', {
-              name: 'tip',
-              data: '图片不得超过8M',
-            });
-            return;
-          }
-          this.fetchUpload(result);
-        })
-        .catch((err) => {
-          this.$bus.$emit('showPopup', {
-            name: 'tip',
-            data: 'yasuocuowu',
-          });
-          console.log(err.message);
-        });
-      return;
-    }
-    this.fetchUpload(file);
+	    if (errorMsg) {
+	      this.$bus.$emit('showPopup', {
+	        name: 'tip',
+	        data: errorMsg,
+	      });
+	      e.target.value = null;
+	      return;
+	    }
+	    if (this.fileType === 'image') {
+	      if (file.size > 8 * 1024 * 1024) {
+	        this.$bus.$emit('showPopup', {
+	          name: 'tip',
+	          data: '图片不得超过8M',
+	        });
+	        return;
+	      }
+	    }
+	    // this.fetchUpload(file);
+	    this.doGet(file).then((res) => {
+		        this.fetchUpload(file,res.token);
+		      });
   	},
-  	fetchUpload(file) {
-      this.$bus.$emit('showPopup', {
-        name: 'loading',
-        data: '文件上传中，请稍等……',
-      });
-      const key = '';
-      const token = '';
-      const putExtra = '';
-      const config = '';
+  	fetchUpload(file,token) {
+  		console.log('fprepare data file:'+ file.name);
+  		console.log('prepare data token:'+ token);
+      const key = file.name;
+      const putExtra = {
+      	fname: "",
+      	params: {},
+      	mimeType: null
+      };
+      var config = {
+        useCdnDomain: true,
+        region: qiniu.region.z2
+      };
+      var requestUrl = qiniu.getUploadUrl(config);
+      console.log('requestUrl:'+requestUrl);
       var observable = qiniu.upload(file,key,token,putExtra,config);
-      var subscription = observable.subscribe(observable);
+      var observer = {
+        next(res){
+        	console.log(res.total.percent + '% 已上传：'+res.total.loaded);
+        },
+        error(err){
+        	console.log(''+err.code+" : "+ err.reqId+" - "+err.message);
+        },
+        complete(res){
+        	console.log('complete:'+res.toString());
+        }
+      }
+      console.log('开始上传。。。');
+      var subscription = observable.subscribe(observer);
+			console.log('subscription = '+subscription);
     },
+    timerPromisefy(delay) {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(timeoutErr);
+        }, delay * 1000);
+      });
+    },
+    doGet(file) {
+      return Promise.race([
+        this.timerPromisefy(15),
+        new Promise((resolve, reject) => {
+          fetch('http://192.168.1.107:3000/auth/H5hhhh', {
+            method: 'GET',
+            headers: {
+              ...requestHeaders,
+            },
+          }).then(response => response.json(),
+          ).then((json) => {
+        		resolve(json);
+      		})
+          .catch((err) => {
+            reject(catchErr);
+          });
+        }),
+      ]).then(value => value).catch(err => err);
+		},
+
 	},
 }
 </script>
